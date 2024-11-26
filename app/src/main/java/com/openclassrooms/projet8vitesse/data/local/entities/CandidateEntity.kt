56643d5238c46a180ec9efefb7a6142f7f92dae5
Bun Nazam <bun.nazam@gmail.com>
2024-11-26 19:57:21 +0100

    git commit -m "Create CandidateEntity for Room"

--- app/src/main/java/com/openclassrooms/projet8vitesse/data/local/entities/CandidateEntity.kt
+++ app/src/main/java/com/openclassrooms/projet8vitesse/data/local/entities/CandidateEntity.kt
@@ -1,4 +1,19 @@
 package com.openclassrooms.projet8vitesse.data.local.entities
 
-class CandidateEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Représente un candidat dans la base de données
+@Entity(tableName = "candidates")
+data class CandidateEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val firstName: String,
+    val lastName: String,
+    val photoUrl: String?,
+    val phoneNumber: String,
+    val email: String,
+    val dateOfBirth: String,
+    val expectedSalary: Double,
+    val notes: String?,
+    val isFavorite: Boolean = false
+)
