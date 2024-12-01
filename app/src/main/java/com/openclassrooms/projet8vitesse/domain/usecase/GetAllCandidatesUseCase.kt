3d5c1d6ed8e2dbff1a8b057429513a7243a9a90b
Bun Nazam <bun.nazam@gmail.com>
2024-12-01 01:35:53 +0100

    Add GetAllCandidatesUseCase

---
+++ app/src/main/java/com/openclassrooms/projet8vitesse/domain/usecase/GetAllCandidatesUseCase.kt
@@ -0,0 +1,27 @@
+package com.openclassrooms.projet8vitesse.domain.usecase
+
+import com.openclassrooms.projet8vitesse.data.repository.CandidateRepository
+import com.openclassrooms.projet8vitesse.domain.model.Candidate
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/**
+ * UseCase pour récupérer tous les candidats.
+ *
+ * Cette classe encapsule la logique métier pour obtenir la liste
+ * complète des candidats depuis le repository.
+ *
+ * @property repository Le repository gérant les opérations sur les candidats.
+ */
+class GetAllCandidatesUseCase @Inject constructor(
+    private val repository: CandidateRepository
+){
+    /**
+     * Exécute le UseCase pour récupérer tous les candidats.
+     *
+     * @return Un flux contenant la liste des candidats.
+     */
+    fun execute(): Flow<List<Candidate>> {
+        return repository.getAllCandidates()
+    }
+}
